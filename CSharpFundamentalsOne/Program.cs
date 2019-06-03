c569a38ea307b16895586167b766516ccadf1251
mbower386 <mbower386@gmail.com>
2019-06-03 16:14:47 -0700

    finished

---
+++ CSharpFundamentalsOne/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace CSharpFundamentalsOne
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            // #1
+            for (int i = 1; i <= 255; i++)
+            {
+                Console.WriteLine (i);
+            }
+
+            // #2
+            for (int j = 0; j <= 100; j++)
+            {
+                if (j % 3 == 0 || j % 5 == 0)
+                {
+                    if (!(j % 3 == 0 && j % 5 == 0))
+                    {
+                        Console.WriteLine (j);
+                    }
+
+                }
+            }
+
+            // #3
+            for (int k = 0; k <= 100; k++)
+            {
+                if ((k % 3 == 0) && (k % 5 != 0))
+                {
+                    Console.WriteLine ("Fizz");
+                }
+                if ((k % 3 != 0) && (k % 5 == 0))
+                {
+                    Console.WriteLine ("Buzz");
+                }
+                if ((k % 3 == 0) && (k % 5 == 0))
+                {
+                    Console.WriteLine ("FizzBuzz");
+                }
+            }
+
+            // #4
+            int l = 1;
+            while (l <= 100)
+            {
+                if ((l % 3 == 0) && (l % 5 != 0))
+                {
+                    Console.WriteLine ("Fizz");
+                }
+                if ((l % 3 != 0) && (l % 5 == 0))
+                {
+                    Console.WriteLine ("Buzz");
+                }
+                if ((l % 3 == 0) && (l % 5 == 0))
+                {
+                    Console.WriteLine ("FizzBuzz");
+                }
+
+                l++;
+            }
+
+        }
+    }
+}
